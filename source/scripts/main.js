47e130d4ba660a0f9dcd5379e556badced8b3033
TJ Potts <tjpotts5@gmail.com>
2015-09-07 14:54:47 -0700

    Added basic WebGL functionality.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,5 +1,24 @@
 $(function() {
-	var canvas = $("#canvas")[0];
-	var gl = twgl.getWebGLContext(canvas);
-	console.log(gl);
+	// Initial setup
+	var gl = twgl.getWebGLContext($("canvas")[0]);
+	var programInfo = twgl.createProgramInfo(gl, ["bilateral-vs", "bilateral-fs"]);
+	var arrays = {
+    	position: { numComponents: 2, data: [
+    		-1, -1,
+    		 1, -1,
+    		-1, 1,
+    		-1, 1,
+    		1, -1,
+    		1, 1,
+    	]}
+    };
+    var bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+ 
+ 	// Repeat every update
+	twgl.resizeCanvasToDisplaySize(gl.canvas);
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+	gl.useProgram(programInfo.program);
+	twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+	twgl.drawBufferInfo(gl, gl.TRIANGLES, bufferInfo);
 });
