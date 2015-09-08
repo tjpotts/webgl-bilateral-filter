96cc9bf82843dc8b0e34618780ab1e2bdedadded
TJ Potts <tjpotts5@gmail.com>
2015-09-07 19:34:18 -0700

    Added texture capability to shaders.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,23 +4,37 @@ $(function() {
 	var programInfo = twgl.createProgramInfo(gl, ["bilateral-vs", "bilateral-fs"]);
 	var arrays = {
     	position: { numComponents: 2, data: [
-    		-100, -100,
-    		 100, -100,
-    		-100, 100,
-    		-100, 100,
-    		100, -100,
+    		0, 0,
+    		100, 0,
+    		0, 100,
+    		0, 100,
+    		100, 0,
     		100, 100,
+    	]},
+    	texCoord: { numComponents: 2, data: [
+    		0, 0,
+    		1, 0,
+    		0, 1,
+    		0, 1,
+    		1, 0,
+    		1, 1
     	]}
     };
+    twgl.setAttributePrefix('a_');
     var bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
  
- 	// Repeat every update
-	twgl.resizeCanvasToDisplaySize(gl.canvas);
-	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+ 	var texture = twgl.createTexture(gl,{src: 'images/birds.png'},function() {
+ 		// Repeat every update
+		twgl.resizeCanvasToDisplaySize(gl.canvas);
+		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-	gl.useProgram(programInfo.program);
-	twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+		gl.useProgram(programInfo.program);
+		twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
 
-	twgl.setUniforms(programInfo,{u_resolution:[gl.canvas.width,gl.canvas.height]})
-	twgl.drawBufferInfo(gl, gl.TRIANGLES, bufferInfo);
+		twgl.setUniforms(programInfo,{
+			u_resolution: [gl.canvas.width,gl.canvas.height],
+			u_texture: texture
+		})
+		twgl.drawBufferInfo(gl, gl.TRIANGLES, bufferInfo);
+ 	});
 });
