37926183d60d022122474d6d61504ffc31944950
TJ Potts <tjpotts5@gmail.com>
2015-09-08 11:26:25 -0700

    Implemented Gaussian blur filter.

--- source/scripts/gaussian.js
+++ source/scripts/gaussian.js
@@ -1,16 +1,29 @@
+function gaussian2D(x,y,ux,uy,sigx,sigy) {
+	var exp = Math.pow(x-ux,2) / (2.0*Math.pow(sigx,2)) + Math.pow(y-uy,2) / (2.0*Math.pow(sigy,2));
+	//return 1;
+	return Math.pow(Math.E,exp*-1);
+}
+
 function generateGaussianMask(size) {
 	var halfSize = Math.floor(size/2);
-	var mask = [];
+	var maskValues = [];
 	var val = 255 / Math.pow(size,2);
+	var sum = 0;
 	for (var x = -halfSize; x <= halfSize; x++) {
 		for (var y = -halfSize; y <= halfSize; y++) {
-			mask.push(val,val,val,255);
+			val = gaussian2D(x,y,0,0,1,1);
+			sum += val;
+			maskValues.push(val);
 		}
 	}
-	console.log(mask.length)
+	var mask = [];
+	for (var i = 0; i < maskValues.length; i++) {
+		val = maskValues[i] / sum * 255;
+		mask.push(val,val,val,255);
+	}
 	return mask;
 }
 
 $(function() {
-	console.log(generateGaussianMask(5));
+	generateGaussianMask(5);
 });
