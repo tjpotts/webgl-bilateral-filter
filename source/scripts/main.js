a7eb6359b15e5a92005ca8520982ad61a2b285d2
TJ Potts <tjpotts5@gmail.com>
2015-09-10 10:44:37 -0700

    Fixed lookup texture wrapping.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -34,7 +34,11 @@ $(function() {
 			u_resolution: [gl.canvas.width,gl.canvas.height],
 			u_texture: texture,
 			u_gaussian: lookupTex
-		})
+		});
+		console.log(gl.canvas.width,gl.canvas.height);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
 		twgl.drawBufferInfo(gl, gl.TRIANGLES, bufferInfo);
 	});
 });
