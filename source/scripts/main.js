d05c7762424eebf0a7b33eeb791244fc90c3eff0
TJ Potts <tjpotts5@gmail.com>
2015-09-13 16:38:50 -0700

    Converted to a React.js app.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,10 +1,13 @@
 "use strict";
 
-$(function() {
-	var gl;
-	var filt;
-
-	function loadShaders(cb) {
+var FilterApp = React.createClass({
+	gl: null,
+	getInitialState: function() {
+		return {
+			initialized: false
+		};
+	},
+	loadShaders: function(cb) {
 		var shaders = $('script[type="x-shader/x-vertex"][src],script[type="x-shader/x-fragment"][src]');
 		var shadersLoaded = 0;
 		shaders.each(function(i, s) {
@@ -15,33 +18,67 @@ $(function() {
 					cb();
 			});
 		});
-	}
-
-	function init() {
-		// Initial setup
-		gl = twgl.getWebGLContext($("canvas")[0]);
+	},
+	init: function() {
+		var canvas = this.refs.canvas.getDOMNode();
+		this.gl = twgl.getWebGLContext($("canvas")[0]);
 		
-		filt = new BilatFilter(gl);
-		filt.init();
-	}
+		this.filt = new BilatFilter(this.gl);
+		this.filt.init();
+	},
+	run: function() {
+		var imgUrl = $('input[name="imgUrl"]').val();
+		var self = this;
 
-	function run() {
-		var imgUrl = $('input[name="imageUrl"]').val();
-
-		twgl.createTexture(gl,{src: imgUrl, wrap: gl.CLAMP_TO_EDGE},function(err, texture, img) {
+		twgl.createTexture(this.gl,{src: imgUrl, wrap: this.gl.CLAMP_TO_EDGE},function(err, texture, img) {
 			var options = {
 				width: img.width,
 				height: img.height
 			};
-			texture = filt.filter(texture,options);
-			filt.draw(texture,options);
+			texture = self.filt.filter(texture,options);
+			self.filt.draw(texture,options);
 		});
 
 		return false;
+	},
+	componentDidMount: function() {
+		var self = this;
+		this.loadShaders(function(){
+			self.init();
+			$("#optionsFrm").submit(self.run);
+		});
+	},
+	render: function() {
+		return (
+			<div>
+				<FilterApp.Form />
+				<FilterApp.Canvas ref="canvas" />
+			</div>
+		);
 	}
-
-	loadShaders(function() {
-		init();
-		$("#optionsForm").submit(run);
-	});
 });
+
+FilterApp.Form = React.createClass({
+	render: function() {
+		return (
+			<form id="optionsFrm">
+				<label htmlFor="imgUrl">Image URL:</label>
+				<input name="imgUrl" type="text" />
+				<button id="runButton" type="submit">Run</button>
+			</form>
+		);
+	}
+});
+
+FilterApp.Canvas = React.createClass({
+	render: function() {
+		return (
+			<canvas />
+		);
+	}
+});
+
+React.render(
+	<FilterApp />,
+	document.getElementById('app')
+);
